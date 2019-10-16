24dc343c6f660f75f4cc4a64a1632d90bf5792ec
Thomas Banks <thomas@tombanks.me>
2019-10-16 11:57:48 +0100

    Initial commit

---
+++ tombanksme/parser.py
@@ -0,0 +1,5 @@
+class Parser:
+
+    def __init__ ( self , location , dev=False ):
+        self.dev        =   dev
+        self.location   =   location
---
+++ uniplot.py
@@ -0,0 +1,2 @@
+from tombanksme.parser import Parser
+
